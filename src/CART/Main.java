d9e1a2dd025974b9e5b2135088fde12c3c628259
rachelmoore212 <moorer2@carleton.edu>
2017-05-25 16:33:19 -0500

    Dictionary of dictionary started

--- src/CART/Main.java
+++ src/CART/Main.java
@@ -22,34 +22,69 @@ public class Main {
 
         }
     }*/
-    public void firstPass(DataSource d){
+    public void mapData(List<DataSource.Datapoint> datapoints){
+        // Our data
 
-        List<DataSource.Datapoint> datapoints = d.getData();
 
+        // Our dictionary that maps index of a category to a dictionary of values in that category
+        // to data that matches it
         Map<Integer,Map<String,List<DataSource.Datapoint>>> categories_to_points = new Hashtable();
 
+        Map<Integer,Map<String, int[]>> categories_to_classifications = new Hashtable<>();
+
+        // Add each datapoint to the dictionary
         for (DataSource.Datapoint data: datapoints){
             List<String> cat_data = data.getCategoricalData();
             for (int i = 0; i < cat_data.size(); i++){
+
+                // Check if category is a key
                 if(categories_to_points.containsKey(i)){
 
                     Map<String,List<DataSource.Datapoint>> value_map = categories_to_points.get(i);
                     String value = cat_data.get(i);
+
+                    // Check if value at category is a key
                     if(value_map.containsKey(value)) {
                         List<DataSource.Datapoint> object_list = value_map.get(value);
                         object_list.add(data);
+                    // add it if its not
                     } else {
                         List<DataSource.Datapoint> new_list = new ArrayList<>();
                         new_list.add(data);
                         value_map.put(value, new_list);
-
                     }
+                 // if category is not a key already, add it
                 } else {
                     Map<String, List<DataSource.Datapoint>> new_map = new Hashtable<>();
                     String value = cat_data.get(i);
                     List<DataSource.Datapoint> new_list = new ArrayList<>();
                     new_map.put(value, new_list);
                     categories_to_points.put(i, new_map);
+                }
+
+                // Check if category is a key
+                if(categories_to_classifications.containsKey(i)){
+
+                    Map<String, int[]> classify_map = categories_to_classifications.get(i);
+                    String value = cat_data.get(i);
+
+                    // Check if value at category is a key
+                    if(classify_map.containsKey(value)) {
+                        int[] classify_array = classify_map.get(value);
+                        //if cat_data.get
+                        // add it if its not
+                    } else {
+                        int [] new_list = new int [] {0,0};
+                        //new_list.add(data);
+                        //classify_map.put(value, new_list);
+                    }
+                    // if category is not a key already, add it
+                } else {
+                    Map<String, int[]> new_map = new Hashtable<>();
+                    String value = cat_data.get(i);
+                    List<DataSource.Datapoint> new_list = new ArrayList<>();
+                    //new_map.put(value, new_list);
+                   // categories_to_points.put(i, new_map);
                 }
 
             }
