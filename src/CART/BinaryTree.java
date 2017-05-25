b5a84d012f9ceb8ccf3312d4af1b2c4562b1111f
rachelmoore212 <moorer2@carleton.edu>
2017-05-25 11:06:24 -0500

    fbwekfaew

--- src/CART/BinaryTree.java
+++ src/CART/BinaryTree.java
@@ -11,17 +11,18 @@ import java.util.ArrayList;
 public class BinaryTree {
 
 
-    Node [] node_list = new Node[100];
+    public ArrayList<Node>  node_list = new ArrayList<Node>();
 
-    Node start_node = new Node(true);
+    public Node start_node = new Node(true);
 
-    ArrayList<String> categories = new ArrayList<String>();
+    public ArrayList<String> categories = new ArrayList<String>();
 
-    Integer min_leaf_size = 0;
+    public Integer min_leaf_size = 0;
 
     public BinaryTree(ArrayList<String> categories, int min_leaf_size){
         categories = categories;
         min_leaf_size = min_leaf_size;
+
 
     }
 
@@ -30,9 +31,44 @@ public class BinaryTree {
         return "TBD";
     }
 
-    public void addSplit(Node n, String category, int val){
-        //add split
+    public void addCategoricalSplit(Node node, int category){
+        node.setCategoricalRule(category);
+        Node left_node = new Node(false);
+        Node right_node = new Node(false);
+        node.setLeft_node(left_node);
+        node.setRight_node(right_node);
+        node_list.add(left_node);
+        node_list.add(right_node);
     }
+    public void addNumericSplit(Node node, int value){
+        node.setNumericRule(value);
+        Node left_node = new Node(false);
+        Node right_node = new Node(false);
+        node.setLeft_node(left_node);
+        node.setRight_node(right_node);
+        node_list.add(left_node);
+        node_list.add(right_node);
+    }
+    public boolean traverse(DataSource.Datapoint data){
+
+        Node current = start_node;
+
+        while (current.isLeaf == false){
+
+            if (current.isCategorical == true){
+                int index = current.category_value;
+                //if (data[index] == current.getMove_left_categorical()){
+
+                //}
+            }
+
+
+
+        }
+
+        return false
+    }
+
     public void pruneNode(Node n){
         //prune shit
     }
@@ -42,32 +78,92 @@ public class BinaryTree {
         public boolean isStart = false;
         public boolean isLeaf = true;
         public boolean isCategorical = false;
-        public String move_left_categorical = null;
+        public int category_value = 0;
+        public int numeric_value = 0;
+        public int move_left_categorical = 0;
         public int move_left_less_than = 0;
         public Node left_node;
         public Node right_node;
 
 
-        public Node(boolean isStart){
 
-            isStart = isStart;
+
+        public Node(boolean start){
+
+            isStart = start;
             isLeaf = true;
 
         }
 
-        public void setCategoricalRule(String category){
+        public void setCategoricalRule(int index, int category){
             isLeaf = false;
             isCategorical = true;
+            category_value = index;
             move_left_categorical = category;
 
         }
 
-        private void setNumericRule(int value){
+        private void setNumericRule(int index, int value){
             isLeaf = false;
             isCategorical = false;
+            numeric_value = index;
             move_left_less_than = value;
         }
+        public boolean isStart() {
+            return isStart;
+        }
 
+        public void setStart(boolean start) {
+            isStart = start;
+        }
+
+        public boolean isLeaf() {
+            return isLeaf;
+        }
+
+        public void setLeaf(boolean leaf) {
+            isLeaf = leaf;
+        }
+
+        public boolean isCategorical() {
+            return isCategorical;
+        }
+
+        public void setCategorical(boolean categorical) {
+            isCategorical = categorical;
+        }
+
+        public Integer getMove_left_categorical() {
+            return move_left_categorical;
+        }
+
+        public void setMove_left_categorical(int move_left_categorical) {
+            this.move_left_categorical = move_left_categorical;
+        }
+
+        public int getMove_left_less_than() {
+            return move_left_less_than;
+        }
+
+        public void setMove_left_less_than(int move_left_less_than) {
+            this.move_left_less_than = move_left_less_than;
+        }
+
+        public Node getLeft_node() {
+            return left_node;
+        }
+
+        public void setLeft_node(Node left_node) {
+            this.left_node = left_node;
+        }
+
+        public Node getRight_node() {
+            return right_node;
+        }
+
+        public void setRight_node(Node right_node) {
+            this.right_node = right_node;
+        }
     }
 
 
--- src/CART/Main.java
+++ src/CART/Main.java
@@ -4,6 +4,9 @@ package CART;
  * Created by Rachel on 5/21/2017.
  */
 public class Main {
+    int [] categorical_indexes = new int [] {2,3,4,6,7,8,9,10,11};
+
+    int [] numeric_indexes = new int [] {1,5,12,13,14,15,16,17,18,19,20,21,22,23};
 
     public static void main(String[] args){
 
